8937bf6afa59c444facfdfb4acb970eb06c463e6
Tony Bowden <tony@tmtm.com>
2015-09-13 11:55:07 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,53 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  noko.css('#text table').xpath('tr[td]').each do |tr|
+    tds = tr.css('td')
+    link = tds[0].css('a/@href').text
+    link = URI.join(url, URI.escape(link)).to_s unless link.to_s.empty?
+
+    data = { 
+      name: tds[0].text.tidy,
+      # role: tds[1].text.tidy,
+      party: tds[2].text.tidy,
+      term: 2012,
+      source: url,
+    }.merge(scrape_person(link))
+    data[:image] = URI.join(url, URI.escape(data[:image])).to_s unless data[:image].to_s.empty?
+    puts data
+    ScraperWiki.save_sqlite([:name, :term], data)
+  end
+end
+
+def scrape_person(url)
+  return {} if url.to_s.empty?
+  noko = noko_for(url)
+  box = noko.css('#main4')
+  data = { 
+    image: box.css('img/@src').text,
+    area: box.xpath('.//strong[text()="Distrito:"]/ancestor::td[1]').text.split(':',2).last.to_s.tidy,
+    source: url.to_s,
+  }
+end
+
+scrape_list('http://parlamento.tl/deputadus/2012/pt/deputados.php')
+scrape_list('http://parlamento.tl/deputadus/2012/pt/dp.php')
