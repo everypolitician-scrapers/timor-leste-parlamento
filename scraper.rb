b180db211940362783f1c550506dae221eeda1f7
Tony Bowden <tony@tmtm.com>
2017-01-15 08:47:59 +0000

    Clean out database on each run

--- scraper.rb
+++ scraper.rb
@@ -44,5 +44,6 @@ def scrape_person(url)
   }
 end
 
+ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
 scrape_list('http://parlamento.tl/deputadus/2012/pt/deputados.php')
 scrape_list('http://parlamento.tl/deputadus/2012/pt/dp.php')
