13b2f70f3b05d99705cbc76c66eccffd9a85cfaf
Tony Bowden <tony@tmtm.com>
2018-08-24 10:33:42 +0100

    Switch to https

--- scraper.rb
+++ scraper.rb
@@ -46,4 +46,4 @@ def scrape_person(url)
 end
 
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
-scrape_list('http://www.parlamento.tl/deputadus/2012-2017/pt/index.php')
+scrape_list('https://www.parlamento.tl/deputadus/2012-2017/pt/index.php')
